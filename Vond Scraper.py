32aa5a85d3289632b03671d7a0d1fe6a9f5ea335
Hamad Alafeefe <60110135+DmarZX2@users.noreply.github.com>
2020-11-18 20:20:42 +0400

    Final Changes

--- Vond Scraper.py
+++ Vond Scraper.py
@@ -5,8 +5,7 @@ title = """
   \ V / (_) | | | | (_| | _\ \ (__| | | (_| | |_) |  __/ |
    \_/ \___/|_| |_|\__,_| \__/\___|_|  \__,_| .__/ \___|_|
                                             |_| V1
-Made by VonD
-BTC: 1Hexziesm44sETfs3BZfwrB81Q4RvBkCQG
+Made by DmarZX2
 
 """
 
